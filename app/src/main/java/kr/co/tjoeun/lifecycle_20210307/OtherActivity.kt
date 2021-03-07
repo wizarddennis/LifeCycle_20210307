6a36e152a0206a0866102f6074c5b8430a446af0
dennis <travlInfo4you@gmail.com>
2021-03-07 09:47:12 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20210307/OtherActivity.kt
@@ -0,0 +1,11 @@
+package kr.co.tjoeun.lifecycle_20210307
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+    }
+}
