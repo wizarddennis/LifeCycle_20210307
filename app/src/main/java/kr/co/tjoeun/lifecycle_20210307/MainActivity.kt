85119c8fd1df7afbc916cf88cead7eb09574435b
dennis <travlInfo4you@gmail.com>
2021-03-07 10:31:38 +0900

    BaseActivity 추가

--- app/src/main/java/kr/co/tjoeun/lifecycle_20210307/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20210307/MainActivity.kt
@@ -1,7 +1,9 @@
 package kr.co.tjoeun.lifecycle_20210307
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,8 +11,25 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        moveToOtherBtn.setOnClickListener {
+        Log.d("메인화면", "onCreate실행됨")
 
-        }
+        moveToOtherBtn.setOnClickListener {
+            val myIntent = Intent(this, OtherActivity::class.java)
+            startActivity(myIntent)
+
+        }      
+        
+    }
+
+    override fun onResume() {
+        super.onResume()
+        
+        Log.d("메인화면", "onResume실행됨")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        
+        Log.d("메인화면", "onPause 실행됨")
     }
 }
--- app/src/main/java/kr/co/tjoeun/lifecycle_20210307/OtherActivity.kt
+++ app/src/main/java/kr/co/tjoeun/lifecycle_20210307/OtherActivity.kt
@@ -2,10 +2,13 @@ package kr.co.tjoeun.lifecycle_20210307
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class OtherActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_other)
+        
+        Log.d("다른화면", "onCreate실행됨")
     }
 }
